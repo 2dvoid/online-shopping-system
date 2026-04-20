cc7b4477c3a5e1d03c3a39dbb9fd7331b389e2e7
2dvoid <2dvoid@mail.com>
2026-04-21 01:16:02 +0600

    Fixed Admin Login

--- database/onlineshop.sql
+++ database/onlineshop.sql
@@ -49,7 +49,7 @@ CREATE TABLE `admin_info` (
 --
 
 INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
-(1, 'admin', 'admin@gmail.com', '12334');
+(1, 'admin', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b');
 
 -- --------------------------------------------------------
 
